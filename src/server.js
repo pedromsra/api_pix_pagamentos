c32e17efd462cc5f0ea0de55e7d29124c26f7f96
Pedro de Araújo <pedromsra@gmail.com>
2023-03-17 14:34:03 -0300

    gerando cobrança

--- src/server.js
+++ src/server.js
@@ -23,12 +23,36 @@ axios({
     url: `${process.env.GN_ENDPOINT}/oauth/token`,
     headers: {
         Authorization: `Basic ${credentials}`,
-        "Content-type": "application/json",
+        "Content-Type": "application/json",
     },
     httpsAgent: agent,
     data: {
         grant_type: "client_credentials"
     }
-}).then((response) => console.log(response.data))
+}).then((response) => {
+    const accessToken = response.data?.access_token;
+
+    const reqGN = axios.create({
+        baseURL: process.env.GN_ENDPOINT,
+        httpsAgent: agent,
+        headers : {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json"
+        }
+    });
+
+    const dataCob = {
+        calendario: {
+            expiracao: 3600
+        },
+        valor: {
+            original: "1.00"
+        },
+        chave: "pedromsra@gmail.com",
+        solicitacaoPagador: "Referente à nossa última sessão"
+    }
+
+    reqGN.post("/v2/cob", dataCob).then((response) => console.log(response.data))
+})
 
 
